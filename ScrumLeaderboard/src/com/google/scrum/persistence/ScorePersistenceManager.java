72656f2b87c8a6a1b15eb150642935a21f9680a8
churro1983 <churro1983@gmail.com>
2015-05-06 15:49:40 +0100

    Adding persistence skeleton

---
+++ ScrumLeaderboard/src/com/google/scrum/persistence/ScorePersistenceManager.java
@@ -0,0 +1,16 @@
+package com.google.scrum.persistence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScorePersistenceManager {
+	
+	public void saveScore(Score score) {
+		
+	}
+	
+	public List<Score> listScores() {
+		return new ArrayList<Score>();
+	}
+
+}
